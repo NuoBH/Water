3e98e537fa796692f11a87ee793e5407cd6cbf0c
NuoBH <36141903+NuoBH@users.noreply.github.com>
2021-04-23 02:54:50 -0700

    chat cube

---
+++ water/cube/ChatCube.js
@@ -0,0 +1,8 @@
+class ChatCube{
+
+}
+
+requestTimeout(function(){
+    var chatCube = document.getElementById("chat-cube");
+    // chatCube.style.setProperty("--faceWidthTwo", "70vmin");
+}, 1100);
