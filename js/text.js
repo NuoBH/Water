e59b4eda9ae95a29f3ae2172355c62de17e44bad
0buo <64718986+0buo@users.noreply.github.com>
2021-04-25 04:23:47 -0700

    1

--- js/text.js
+++ js/text.js
@@ -2,7 +2,27 @@ class CubeContent{
     constructor(){
 
     }
+    
+    addTitle(face, str, factor=1.75){
+        let textDOM = face.children[0];
 
+        let titleDOM = document.createElement(`span`);
+        titleDOM.classList.add(`title`);
+        titleDOM.innerHTML = str;
+        textDOM.prepend(titleDOM);
+
+        let fontSize = chatCube.targetFaceHeightFour / factor
+        titleDOM.style.setProperty(`font-size`, `${fontSize}px`);
+        console.log(fontSize);
+
+        window.addEventListener(`resize`, ()=>{this.resizeTitle(titleDOM, factor)});
+    }
+
+    resizeTitle(title, factor){
+        console.log(chatCube.targetFaceHeightFour)
+        title.style.setProperty(`font-size`, `${chatCube.targetFaceHeightFour / factor}px`);
+    }
 }
 
 var cubeContent = new CubeContent();
+cubeContent.addTitle(frontFace, `mate<br>rials`, 1.75);
