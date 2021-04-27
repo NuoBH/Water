3530f52c1210fdb04d068421839f3576de127d1d
0buo <64718986+0buo@users.noreply.github.com>
2021-04-27 01:14:12 -0700

    1

--- js/text.js
+++ js/text.js
@@ -6,7 +6,7 @@ class CubeContent{
     addTitle(face, str, factor=1.75){
         let textDOM = face.children[0];
 
-        let titleDOM = document.createElement(`span`);
+        let titleDOM = document.createElement(`div`);
         titleDOM.classList.add(`title`);
         titleDOM.innerHTML = str;
         textDOM.prepend(titleDOM);
@@ -22,7 +22,30 @@ class CubeContent{
         console.log(chatCube.targetFaceHeightFour)
         title.style.setProperty(`font-size`, `${chatCube.targetFaceHeightFour / factor}px`);
     }
+
+    addChat(face, str){
+        let textDOM = face.children[0];
+
+        let chatDOM = document.createElement(`p`);
+        chatDOM.classList.add(`paragraph`);
+        chatDOM.classList.add(`chat`);
+        chatDOM.innerHTML = str;
+        textDOM.append(chatDOM);
+
+        $(textDOM).animate({
+            scrollTop: textDOM.scrollHeight
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+
+        chatDOM.classList.add(`send`);
+    }
 }
 
 var cubeContent = new CubeContent();
 cubeContent.addTitle(frontFace, `mate<br>rials`, 1.75);
+
+window.addEventListener(`mouseup`, function(){
+    cubeContent.addChat(frontFace, `Water is an imaginary data structure.`);
+});
