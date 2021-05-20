3379addc2c1c37b1b704ce295f99fc7192561f53
NuoBH <36141903+NuoBH@users.noreply.github.com>
2021-05-20 06:54:32 -0700

    b

---
+++ js/archive.js
@@ -0,0 +1,12 @@
+function addArchivePage1(face){
+    cubeContent.addChat(face, "Data Pick Up Date: May 06 2021.");
+    cubeContent.addChat(face, "Ice Delivery Date: May 11 2021.");
+    addImageCube("archiveImageCube1", face, [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ]);
+}
