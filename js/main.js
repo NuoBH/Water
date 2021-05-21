f290f71f10e44c09c8a2357c88c280f3ec8c972d
NuoBH <36141903+NuoBH@users.noreply.github.com>
2021-05-20 17:03:17 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -53,34 +53,34 @@ cubeContent.addTitle(frontFace, `Water`, 1.8);
 
 /************ uncomment code below after test is done ***********/
 
-// window.addEventListener(`firstCollide`, function(){
-//     requestTimeout(function(){
-//         cubeContent.addChat(frontFace, `<b>Water</b> is an unstable data structure with which you store, share and delete your data.`);
-//     }, 1000);
-//     requestTimeout(function(){
-//         let optionDOM = optionCreator.addOptionButtons(frontFace, 
-//                         [next]);
+window.addEventListener(`firstCollide`, function(){
+    requestTimeout(function(){
+        cubeContent.addChat(frontFace, `<b>Water</b> is an unstable data structure with which you store, share and delete your data.`);
+    }, 1000);
+    requestTimeout(function(){
+        let optionDOM = optionCreator.addOptionButtons(frontFace, 
+                        [next]);
             
-//         addEventHandlerToButtons(optionDOM, optionEventName, waterOption0);
-//     }, 2500);
+        addEventHandlerToButtons(optionDOM, optionEventName, waterOption0);
+    }, 2500);
     
-// });
+});
 
 /** **************************************************** */
 
 /**test part delete later */
-window.addEventListener(`firstCollide`, function(){
-    requestTimeout(function(){
-        cubeContent.addTitle(leftFace, "Send<br>Water");
-        chatCube.rotate();
-        chatCube.rotate();
-        chatCube.rotate();
-        chatCube.rotate();
-        // chatCube.rotate();
+// window.addEventListener(`firstCollide`, function(){
+//     requestTimeout(function(){
+//         cubeContent.addTitle(leftFace, "Send<br>Water");
+//         chatCube.rotate();
+//         chatCube.rotate();
+//         chatCube.rotate();
+//         chatCube.rotate();
+//         // chatCube.rotate();
 
-     setupStart();
-    }, 1000);
-});
+//      setupStart();
+//     }, 1000);
+// });
 
 
 /************************** */
